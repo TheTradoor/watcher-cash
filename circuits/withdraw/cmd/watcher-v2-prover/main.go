ecddb1bf522a9fcf3b85012ba20f376229494902
TheTradoor <bollish.sol@gmail.com>
2026-08-29 17:47:14 +0700

    Add Watcher V2 local prover CLI

---
+++ circuits/withdraw/cmd/watcher-v2-prover/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	withdraw "watcher.cash/circuits/withdraw"
+)
+
+func main() {
+	bundleDir := flag.String("bundle", "fixture-out/v2", "V2 proving bundle directory")
+	circuit := flag.String("circuit", "", "deposit or withdraw")
+	witnessPath := flag.String("witness", "", "witness JSON file; defaults to stdin")
+	flag.Parse()
+	if *circuit != "deposit" && *circuit != "withdraw" {
+		fmt.Fprintln(os.Stderr, "--circuit must be deposit or withdraw")
+		os.Exit(2)
+	}
+	bundle, err := withdraw.LoadProverBundleV2(*bundleDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "load V2 prover bundle:", err)
+		os.Exit(1)
+	}
+	var data []byte
+	if *witnessPath == "" {
+		data, err = io.ReadAll(os.Stdin)
+	} else {
+		data, err = os.ReadFile(*witnessPath)
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "read witness:", err)
+		os.Exit(1)
+	}
+	var response withdraw.ProofResponseV1
+	if *circuit == "deposit" {
+		response, err = bundle.ProveDepositJSON(data)
+	} else {
+		response, err = bundle.ProveWithdrawJSON(data)
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "prove V2", *circuit+":", err)
+		os.Exit(1)
+	}
+	encoder := json.NewEncoder(os.Stdout)
+	if err := encoder.Encode(response); err != nil {
+		fmt.Fprintln(os.Stderr, "encode response:", err)
+		os.Exit(1)
+	}
+}
