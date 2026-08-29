781473520af6d8e7677ada42007373606bf48ed8
TheTradoor <bollish.sol@gmail.com>
2026-08-29 23:58:22 +0700

    Scope hardened wallet transport to V2 route

---
+++ app/v2/layout.jsx
@@ -0,0 +1,7 @@
+'use client';
+
+import V2WalletTransport from './V2WalletTransport';
+
+export default function WatcherV2Layout({ children }) {
+  return <V2WalletTransport>{children}</V2WalletTransport>;
+}
