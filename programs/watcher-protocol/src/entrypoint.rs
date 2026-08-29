1db930b2f4802fe2b50d5c350ed955421c9ec78e
TheTradoor <bollish.sol@gmail.com>
2026-08-29 17:46:29 +0700

    Dispatch isolated V2 instruction tags

--- programs/watcher-protocol/src/entrypoint.rs
+++ programs/watcher-protocol/src/entrypoint.rs
@@ -3,5 +3,10 @@ use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubke
 solana_program::entrypoint!(process_entrypoint);
 
 fn process_entrypoint(program_id: &Pubkey, accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
-    crate::processor::process_instruction(program_id, accounts, data)
+    match data.first().copied() {
+        Some(0x20 | 0x21 | 0x22) => {
+            crate::v2::processor::process_instruction_v2(program_id, accounts, data)
+        }
+        _ => crate::processor::process_instruction(program_id, accounts, data),
+    }
 }
